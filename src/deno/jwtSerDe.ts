ed597e76e93d90b67d730620dc08c25b6f2d3bda
Arjan Molenaar <gaphor@gmail.com>
2022-10-26 17:28:12 +0200

    Remove writing key.json

--- src/deno/jwtSerDe.ts
+++ src/deno/jwtSerDe.ts
@@ -7,21 +7,9 @@ const keyUsages: KeyUsage[] = ["sign", "verify"];
 let jsonWebKey: JsonWebKey;
 let key: CryptoKey;
 try {
-  try {
-    // try to load key from local file
-    const data = Deno.readFileSync("./key.json");
-    jsonWebKey = JSON.parse(new TextDecoder().decode(data));
-    key = await crypto.subtle.importKey("jwk", jsonWebKey, algo, true, keyUsages);
-  } catch (_e) {
-    // if key.json doesn't exist, generate a new key and save it to key.json
-    key = await crypto.subtle.generateKey(algo, true, keyUsages);
-    jsonWebKey = await crypto.subtle.exportKey("jwk", key);
-    try {
-      Deno.writeFileSync("./key.json", new TextEncoder().encode(JSON.stringify(jsonWebKey)));
-    } catch (_e) {
-      console.error("Could not save key.json to disk");
-    }
-  }
+  // if key.json doesn't exist, generate a new key and save it to key.json
+  key = await crypto.subtle.generateKey(algo, true, keyUsages);
+  jsonWebKey = await crypto.subtle.exportKey("jwk", key);
 } catch (e) {
   console.error(e);
   throw new Error("Unable to load or generate key", e);
